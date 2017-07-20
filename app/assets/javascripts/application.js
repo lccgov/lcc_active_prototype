884053e69613686b096694aa2da66c070ff21f37
David Hunter <david.hunter@leeds.gov.uk>
2017-07-20 09:59:09 +0100

    updated packages and sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,4 @@
 
 $('.header-right').append('<div class="col-xs-12 nav-btns"><a target="_blank" rel="external" href="http://sport.leeds.gov.uk/memberships/introduction.aspx" class="btn btn-lg pull-right btn-secondary nav-btn"><span class="sr-only">External link opens in new window </span>Join Today</a><a target="_blank" rel="external" href="https://sport.leeds.gov.uk/onlinebookings" class="btn btn-lg pull-right btn-main nav-btn"><span class="sr-only">External link opens in new window </span>Book Online</a></div>');
+
+//= require google-analytics
