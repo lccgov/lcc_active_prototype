0b81044e9091e974218430756fcdc9386bc49fbf
David Hunter <david.hunter@leeds.gov.uk>
2018-03-01 11:07:16 +0000

    updated active packages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,3 @@
 $('.header-right').append('<div class="col-xs-12 nav-btns"><a target="_blank" rel="external" href="http://sport.leeds.gov.uk/memberships/introduction.aspx" class="btn btn-lg pull-right btn-secondary nav-btn"><span class="sr-only">External link opens in new window </span>Join Today</a><a target="_blank" rel="external" href="https://sport.leeds.gov.uk/onlinebookings" class="btn btn-lg pull-right btn-main nav-btn"><span class="sr-only">External link opens in new window </span>Book Online</a></div>');
 
 //= require google-analytics
-
-
-
-   $(document).ready(function(){
-     $('.sectionHeader').append(' <video playsinline autoplay muted loop poster="polina.jpg" id="bgvid">    	<source src="/public/leedsgirlscan/videos/tgc-leeds-stings-dist.webm" type="video/webm">      	<source src="/public/leedsgirlscan/videos/tgc-leeds-stings-dist.mp4" type="video/mp4">      	<source src="/public/leedsgirlscan/videos/tgc-leeds-stings-dist.ogv" type="video/ogv">  </video>     <div class="tagline">				<p>Find out more</p>				<a href="#top">					<div class="tagline__anchor">		<span>	<i class="tagline__arrow fa fa-angle-down fa-3x bounce"></i>	</span>		</div>	</a></div>  ');
-   });
